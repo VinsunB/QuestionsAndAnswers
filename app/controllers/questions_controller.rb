80c44a97adef23cb65555debbd74edf5e1e22bab
VinsunB <vinsuncoding@gmail.com>
2016-04-15 09:22:43 -0400

    questions controller methods created

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,12 +1,11 @@
 class QuestionsController < ApplicationController
- 
+ before_action :find_question, only: [:show, :edit]
 
   def index
   @questions = Question.limit(10)
   end
 
   def show
-  @question = Question.find(params[:id])
   end
 
   def edit
@@ -18,13 +17,21 @@ class QuestionsController < ApplicationController
 
 def create 
 @question = Question.new(question_params)
-
+if @question.save
+  redirect_to @question
+  else
+  render 'new' 
+end
 end
 
 private
 
-def question_params
+def find_question
+@question = Question.find(params[:id])
+end
 
+def question_params
+params.require(:question).permit(:title, :body)
 	end
 
 end
